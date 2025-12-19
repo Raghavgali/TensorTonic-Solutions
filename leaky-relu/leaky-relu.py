367b1951728937796ff357d4d5717d7755c584e4
Raghav G <64415434+Raghavgali@users.noreply.github.com>
2025-12-19 11:31:34 -0500

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -6,6 +6,4 @@ def leaky_relu(x, alpha=0.01):
     """
     x = np.array(x, dtype=float)
 
-    v_func = np.vectorize(lambda x: x if x >= 0 else alpha * x)
-
-    return v_func(x)
+    return np.where(x >= 0, x, alpha * x)
